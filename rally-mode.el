851bcf516bfd8079aa08e934c7b08dfc5ecd29fb
Sean LeBlanc <sean.leblanc@icd-tech.com>
2016-02-22 09:30:00 -0700

    Add more formatting. Extract more functions.

--- rally-mode.el
+++ rally-mode.el
@@ -27,17 +27,20 @@
 
 (require 'url-util)
 
+
 ;;; Code: 
 
 (defvar rally-user)
 (defvar rally-password)
+(defvar rally-tasks-cache nil)
 
 (define-derived-mode rally-mode special-mode "rally-mode"
   "Major mode for interacting with Rally website."
   :group 'rally-mode)
 
 (define-key rally-mode-map (kbd "g") 'rally-current-iteration)
-
+(define-key rally-mode-map (kbd "i") 'rally-get-description)
+(define-key rally-mode-map (kbd "r") 'rally-draw-results)
 
 
 (defvar xyz-block-authorisation nil 
@@ -49,6 +52,12 @@
 
 (ad-activate 'url-http-handle-authentication)
 
+;; Similar to sqlplus' shine-color func:
+(defun rally-shine-color (color change)
+  (apply 'color-rgb-to-hex
+	 (mapcar (lambda (arg) (max 0.0 (min 1.0 (+ arg change))))
+		   (color-name-to-rgb color))))
+
 (defun rally-build-url (url-server-string params)
   (concat url-server-string "?"
 	  (url-build-query-string params)))
@@ -57,6 +66,7 @@
   ;;(princ url) 
   (let ((xyz-block-authorisation t)
 	(url-request-method "GET")
+	(url-queue-timeout 60)
 	(url-request-extra-headers 
 	 `(("Content-Type" . "application/xml")
 	   ("Authorization" . ,(concat "Basic "
@@ -71,7 +81,7 @@
   `(
     (query ,(format "((Owner.Name = %s ) AND (( Iteration.StartDate <= today ) AND (Iteration.EndDate >= today)) )" username ) )
     (order Rank)
-    (fetch "true,WorkProduct,Tasks,Iteration,Estimate,State,ToDo,Name")
+    (fetch "true,WorkProduct,Tasks,Iteration,Estimate,State,ToDo,Name,Description")
     ))
 
 (defun rally-make-query (username password)
@@ -99,6 +109,7 @@
    (assoc 'ToDo lst)
    (assoc 'Estimate lst)
    `(WorkItemName . ,(assoc-val 'FormattedID (find 'WorkProduct lst :key #'car)))
+   `(Description . ,(assoc-val 'Description (find 'WorkProduct lst :key #'car ) ))
    ))
 
 
@@ -122,7 +133,8 @@
 (defun rally-get-buffer ()
   (get-buffer-create "*rally-current-iteration*"))
 
-(defvar rally-line-string "%-6s %-80s %-10s %-4s %-4s\n")
+(defvar rally-line-string nil "Formatting for Rally info.")
+(setq rally-line-string "%-6s %-90.90s %-10s %-4s %-4s\n")
 
 (defun rally-write-task-line (parsed-json)
   (insert (format rally-line-string
@@ -133,27 +145,61 @@
 		  (assoc-val 'ToDo parsed-json)
 		  )))
 
-(defun rally-write-output-to-buffer (buf parsed-json)
-  (with-current-buffer buf 
-    (insert (format rally-line-string
+(defun rally-write-header ()
+  (insert (propertize (format rally-line-string
 		    "Story"
 		    "Description"
 		    "Status"
 		    "Est."
 		    "ToDo"
-		    ))
+		    ) 'face `(:background ,(rally-shine-color (face-background 'default) +.2) :foreground ,(rally-shine-color (face-foreground 'default) +.2) :weight bold ))))
+
+
+(rally-shine-color (face-foreground 'default) -.6)
+(face-background 'default)
+
+(rally-shine-color (face-background 'default) +20)
+(defun rally-write-output-to-buffer (buf parsed-json)
+  (with-current-buffer buf 
+    (rally-write-header)
+		   
     (mapcar #'rally-write-task-line parsed-json)))
+
+(defun rally-draw-results ()
+  (interactive)
+  (let ((rally-current-iteration-buffer (rally-get-buffer)))
+    (switch-to-buffer rally-current-iteration-buffer)
+    (rally-mode)
+    (let (buffer-read-only)
+    (progn          
+      (erase-buffer)
+      (rally-write-output-to-buffer (rally-get-buffer) rally-tasks-cache)))))
+
 
 (defun rally-current-iteration ()
   "Pulls up current iteration information for the supplied user."
   (interactive)
   
-  (let ((rally-current-iteration-buffer (rally-get-buffer)))
-    (switch-to-buffer rally-current-iteration-buffer)
-    (rally-mode)
-    (let (buffer-read-only)
-      (erase-buffer)
-      (rally-write-output-to-buffer rally-current-iteration-buffer (rally-fetch-and-parse-current-iteration-info)))))
+  ;; (let ((rally-current-iteration-buffer (rally-get-buffer)))
+  ;;   (switch-to-buffer rally-current-iteration-buffer)
+  ;;   (rally-mode)
+  ;;   (let (buffer-read-only)
+      (progn 
+	(setq rally-tasks-cache (rally-fetch-and-parse-current-iteration-info))
+	(rally-draw-results)))
+
+	;; (erase-buffer)
+	;; (rally-write-output-to-buffer rally-current-iteration-buffer rally-tasks-cache)))))
+
+(defun rally-extract-description (idx)
+  (assoc 'Description (nth idx rally-tasks-cache)))
+
+(defun rally-get-description ()
+  "Get detailed description of story/defect."
+  (interactive)
+  (print (rally-extract-description (- (line-number-at-pos) 2))))
+
 
 (provide 'rally-mode)
 ;;; rally-mode ends here
+
